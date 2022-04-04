3eba4a9641e167eaea5665e10473d1aec702525a
Kenson Johnson <kenson.johnson@outlook.com>
2022-04-03 20:36:02 -0500

    Added legends to generation of the grid within createGrid()
    createGrid now adds ascending grades across the top row and ascending years
    down the first column. Next step is to generate the incrementing pay in each
    cell.

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 const container = document.getElementById('grid');
 
-function createGrid(rows, cols) {
-    container.style.setProperty('--grid-rows', rows);
-    container.style.setProperty('--grid-cols', cols);
+function createGrid(rows = 20, cols = 5) {
+    container.style.setProperty('--grid-rows', rows + 1);
+    container.style.setProperty('--grid-cols', cols + 1);
 
-    for (i = 0; i < (rows * cols); i++) {
-        let square = document.createElement("div");
-        container.appendChild(square).className = "grid-square";
-        square.innerHTML = i + 1;
+    //run one loop to create legend accross top of grid
+    for (i = 0; i <= cols; i++) {
+
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-cell";
+        if (i == 0) {
+            cell.innerHTML = "Years Served"
+        } else {
+            cell.innerHTML = `Grade ${i}`
+        }
+    };
+
+    //create loop that generates remaining cells
+    for (i = 0; i < rows; i++) {
+        //first create cell for side legend
+        let cell = document.createElement("div");
+        container.appendChild(cell).className = "grid-cell";
+        cell.innerHTML = i + 1;
+        if (i == rows - 1) { cell.innerHTML = `${i+1}+` }
+
+        //now create remaining cells within current row i
+        for (j = 0; j < cols; j++) {
+            let cell = document.createElement("div");
+            container.appendChild(cell).className = "grid-cell";
+            cell.innerHTML = `$${j + 1}`
+        }
     }
 }
 
-createGrid(9, 5)
+createGrid()
