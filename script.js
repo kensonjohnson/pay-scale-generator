043bf7b90541c82b1f59b9c6c019df6e4356745e
Kenson Johnson <kenson.johnson@outlook.com>
2022-04-08 18:45:21 -0500

    Built multi-step form
    Divided the form into three "steps" that act similar to a setup wizard.
    Once all data is inserted and user hits submit, the wizard hides and the
    Pay Scale animates in.
    
    Next: create a restart button and aesthetic upgrades.

--- script.js
+++ script.js
@@ -10,7 +10,11 @@ let companyName;
 let departmentName;
 
 function createHeader() {
-    if (companyName === null) return;
+    //erase all HTML within the header div, in case a header already exists
+    header.innerHTML == '';
+
+    //Use a string literal to create the pay scale title and add it to the DOM
+    if (companyName === '') return;
     let title = document.createElement("div");
     header.appendChild(title).className = "title";
     title.innerHTML = `${companyName} ${departmentName} Pay Scale`
@@ -59,8 +63,10 @@ function createGrid() {
     }
 }
 
-//this sets all of the current user inputs diplayed on screen to the currect
-//variables and makes sure that they are parsed as a number. Then creates new grid.
+// This sets all of the current user inputs diplayed on screen to the currect
+// variables and makes sure that they are parsed as a number where needed. Then 
+// creates new grid. 
+
 function handleForm() {
     rows = parseInt(document.getElementById('rows').value);
     cols = parseInt(document.getElementById('cols').value);
@@ -70,11 +76,62 @@ function handleForm() {
     companyName = document.getElementById('companyName').value;
     departmentName = document.getElementById('departmentName').value;
 
+    currentStep = 3;
     createHeader();
     createGrid();
+    showCurrentStep();
 }
 
 //set event listener for submit button
 submitButton.addEventListener('click', button => {
     handleForm();
 })
+
+
+/*************************************************/
+//
+// This section is to handle the multi-step form
+//
+/*************************************************/
+
+const multiStepForm = document.querySelector("[data-multi-step]")
+const formSteps = [...multiStepForm.querySelectorAll("[data-step]")]
+let currentStep = formSteps.findIndex(step => {
+    return step.classList.contains("active")
+})
+
+if (currentStep < 0) {
+    currentStep = 0
+    showCurrentStep()
+}
+
+multiStepForm.addEventListener("click", e => {
+    let incrementor
+    if (e.target.matches("[data-next]")) {
+        incrementor = 1
+    } else if (e.target.matches("[data-previous]")) {
+        incrementor = -1
+    }
+
+    if (incrementor == null) return
+
+    const inputs = [...formSteps[currentStep].querySelectorAll("input")]
+    const allValid = inputs.every(input => input.reportValidity())
+    if (allValid) {
+        currentStep += incrementor
+        showCurrentStep()
+    }
+})
+
+formSteps.forEach(step => {
+    step.addEventListener("animationend", e => {
+        formSteps[currentStep].classList.remove("hide")
+        e.target.classList.toggle("hide", !e.target.classList.contains("active"))
+    })
+})
+
+function showCurrentStep() {
+    formSteps.forEach((step, index) => {
+        step.classList.toggle("active", index === currentStep)
+    })
+}
